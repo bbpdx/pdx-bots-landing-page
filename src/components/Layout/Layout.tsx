ff7e290d0d45be998480bea1096604b74d8daa94
minoka kakizaki <minoka.kakizaki@gmail.com>
2023-03-28 14:56:32 -0700

    Made a basic header and navigation for pages

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from '@/styles/Layout.module.css';
+import { Box } from '@mui/system';
+
+const Layout = ({ children }: any) => {
+  return (
+    <div>
+      <header>
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          color: 'white',
+          backgroundColor: 'grey',
+          opacity: '0.6',
+        }}>
+          <Link href='/'>
+              <h1>PDX Bots</h1>
+          </Link>
+          <Box sx={{
+              display: 'flex',
+              justifyContent: 'space-evenly',
+              width: '100%',
+              alignItems: 'center',
+              color:'#16FF00',
+              opacity: '1',
+            }}>
+            <Link href="/">Home</Link>
+            <Link href="/about">About Us</Link>
+            <Link href="/contact">Contact Us</Link>
+          </Box>
+        </Box>
+      </header>
+      <main>{children}</main>
+      <footer>
+        {/* Your footer content goes here */}
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '@/styles/Home.module.css'
+
+
+export default function Contact() {
+  return (
+    <main className={styles.main}>
+      <div>contact</div>
+    </main>
+  )
+}
