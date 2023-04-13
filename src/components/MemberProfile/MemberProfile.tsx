4ae14a2545dae9b399b014c5c231cc3d967d75f3
minoka kakizaki <minoka.kakizaki@gmail.com>
2023-04-12 17:44:25 -0700

    trying to add image

--- src/components/MemberProfile/MemberProfile.tsx
+++ src/components/MemberProfile/MemberProfile.tsx
@@ -5,10 +5,15 @@ import { Box, Typography } from '@mui/material'
 export default function MemberProfile( {member} : any) {
 
   return (
-    <Box>
-        <Image src={member.image} alt={member.name} width={200} height={200}/>
-        <Typography>{member.name}</Typography>
-        <Typography>{member.description}</Typography>
+    <Box 
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}>
+        <Image src='' alt={member.name} width={200} height={200}/>
+        <Typography variant='h2'>{member.name}</Typography>
+        <Typography variant='body1'>{member.description}</Typography>
     </Box>
   )
 }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -14,7 +14,7 @@ export const headerNavigation = [{
 
 export const memberProfiles = [{
     name: 'Minoka Kakizaki',
-    profilePic: 'https://i.imgur.com/0y1Z2YR.jpg',
+    image: ''
     position: 'CEO',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing vitae proin sagittis nisl rhoncus mattis. Turpis egestas sed tempus urna et pharetra pharetra massa massa. Enim tortor at auctor urna.'
 },
