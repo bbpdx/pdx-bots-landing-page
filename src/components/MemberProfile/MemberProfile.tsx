202b9570a7af1d6733520354026929624c2a2896
minoka kakizaki <minoka.kakizaki@gmail.com>
2023-04-30 17:26:39 -0700

    adding a troll face as a place holder

--- src/components/MemberProfile/MemberProfile.tsx
+++ src/components/MemberProfile/MemberProfile.tsx
@@ -12,7 +12,18 @@ export default function MemberProfile( {member} : any) {
         justifyContent: 'center',
         border: 'red 1px solid',
       }}>
-        <Image src='' alt={member.name} width={200} height={200}/>
+          <Box sx={{ 
+          width: '10rem',
+          height: '10rem', 
+          position: 'relative',
+          border: 'green 1px solid',
+        }}>
+            <Image
+              src="/Trollface.png" 
+              alt="profile pic"
+              fill
+              />
+          </Box>
         <Typography variant='h2' sx={{color:'grey'}}>{member.name}</Typography>
         <Typography variant='body1'sx={{color:'grey'}}>{member.description}</Typography>
     </Box>
