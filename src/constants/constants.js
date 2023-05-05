703d8592c2378af103005ff996eecf0e9454fd06
minoka kakizaki <minoka.kakizaki@gmail.com>
2023-05-04 19:05:14 -0700

    updated profile constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -13,25 +13,24 @@ export const headerNavigation = [{
 ]
 
 export const memberProfiles = [{
-    name: 'Minoka Kakizaki',
+    name: 'Francesca Frattaroli',
     image: '/profile.png',
     position: 'CEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing vitae proin sagittis nisl rhoncus mattis. Turpis egestas sed tempus urna et pharetra pharetra massa massa. Enim tortor at auctor urna.'
-},
-{
-    name: 'Minoka Kakizaki',
+    description: 'Francesca Frattaroli, BSME - Maker, educator, & lifelong learner based in Portland, OR '
+},{
+    name: 'Morgan Litton',
     image:  '/profile.png',
-    position: 'CEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing vitae proin sagittis nisl rhoncus mattis. Turpis egestas sed tempus urna et pharetra pharetra massa massa. Enim tortor at auctor urna.'
+    position: 'COO',
+    description: 'An electrical engineer in academics, and a software engineer by trade. Morgan has a PhD in electrical engineering, and has been building professional software for over 10 years. He has built and led teams focused on machine learning, robotics, big data, software security, and much more. He currently has every AWS certification, including specialist certifications.'
+},{
+    name: 'Jordan Laurent',
+    image:  '/profile.png',
+    position: 'CFO',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id risus est. Ut mollis nibh pretium, malesuada turpis a, efficitur metus. Fusce fermentum tortor urna, sed egestas ex maximus ac.',
 },{
     name: 'Minoka Kakizaki',
     image:  '/profile.png',
-    position: 'CEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing vitae proin sagittis nisl rhoncus mattis. Turpis egestas sed tempus urna et pharetra pharetra massa massa. Enim tortor at auctor urna.'
-},{
-    name: 'Minoka Kakizaki',
-    image:  '/profile.png',
-    position: 'CEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing vitae proin sagittis nisl rhoncus mattis. Turpis egestas sed tempus urna et pharetra pharetra massa massa. Enim tortor at auctor urna.'
+    position: 'CTO',
+    description: 'I am a full-stack software engineer with a background in audio production and a passion for user-centric UX, who believes in inclusion, accessibility, and test-driven production. I excel in analyzing and adjusting work procedures for adaptable/efficient workflow, and my values center on a holistic technical approach to the digital world.'
 },
 ]
