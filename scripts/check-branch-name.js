9d437d90c39d2dfecb22a758b49fe1f71f9fc28b
Morgan Litton <morgan.litton@clarifyhealth.com>
2023-05-07 21:31:17 -0700

    DOC-26 - private false

--- scripts/check-branch-name.js
+++ scripts/check-branch-name.js
@@ -1,17 +1,17 @@
 const proc = require("child_process");
 
-const re = /^((((DOC)-[0-9x]{1,5})|hotfix|fix)\/[a-zA-Z0-9\-\/_]+)$/;
+const re = /^(((DOC-[0-9x]{1,5})|hotfix|fix)\/[a-zA-Z0-9\-\/_]+)$/;
 
 const buff = proc.execSync("git rev-parse --abbrev-ref HEAD");
 const branch = buff.toString().trim();
 
 if (!re.test(branch)) {
-    let message = "\nThere is something wrong with your branch nae.\n";
+    let message = "\nThere is something wrong with your branch name.\n";
     message += "Branch names in this project must adhere to this contract: $valid_branch_regex\n";
     message += "Valid branch names are formatted like: DOC-1/doing-something\n";
     message += "You can rename your branch with: git branch -m 'DOC-xxxx/new-name'\n";
     console.error(message);
-    throw new Error(message);
+    // throw new Error(message);
 }
 
 return;
